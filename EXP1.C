e5cf3350353ffb14dac907bb49176a8dd884eb74
Aditi Narendran <63870252+lilyblack31@users.noreply.github.com>
2021-09-18 14:44:28 +0530

    Experiment 1 Upload

---
+++ EXP1.C
@@ -0,0 +1,14 @@
+#include<graphics.h>
+#include<conio.h>
+int main(){
+int gd=DETECT,gm;
+initgraph (&gd,&gm,"C:\\TURBOC3\\BGI");
+setbkcolor(BLACK);
+line(50,40,190,40);
+rectangle(70,115,215,165);
+arc(120,200,180,0,30);
+circle(120,270,30);
+ellipse(120,350,0,360,30,20);
+getch();
+return 0;
+}
---
+++ EXP1CB.C
@@ -0,0 +1,27 @@
+#include<graphics.h>
+#include<conio.h>
+#include<dos.h>
+main()
+{
+ int gd = DETECT, gm, x, y, color, angle = 0;
+ struct arccoordstype a, b;
+ initgraph(&gd, &gm, "C:\\TURBOC3\\BGI");
+ delay(2000);
+ while(angle<=360)
+ {
+ setcolor(BLACK);
+ arc(getmaxx()/2,getmaxy()/2,angle,angle+2,100);
+ setcolor(BLUE);
+ getarccoords(&a);
+ circle(a.xstart,a.ystart,25);
+ setcolor(BLACK);
+ arc(getmaxx()/2,getmaxy()/2,angle,angle+2,150);
+ getarccoords(&a);
+ setcolor(YELLOW);
+ circle(a.xstart,a.ystart,25);
+ angle = angle+5;
+ delay(50);
+ }
+ getch();
+ closegraph();
+}
